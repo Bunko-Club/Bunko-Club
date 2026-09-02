8972fcccf47e21b01bf1aab4a16d0dead96f1ec8
Inuk <bastian@inuk.blog>
2026-09-02 19:05:24 +0200

    move from react to angular

--- Bunko-Club.AppHost/AppHost.cs
+++ Bunko-Club.AppHost/AppHost.cs
@@ -4,10 +4,14 @@ var server = builder.AddProject<Projects.Bunko_Club_Server>("server")
     .WithHttpHealthCheck("/health")
     .WithExternalHttpEndpoints();
 
-var webfrontend = builder.AddViteApp("webfrontend", "../frontend")
+var frontend = builder.AddJavaScriptApp("frontend", "../Bunko-Club.Frontend", runScriptName: "start")
+    .WithNpm(installCommand: "ci")
     .WithReference(server)
-    .WaitFor(server);
+    .WaitFor(server)
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
 
-server.PublishWithContainerFiles(webfrontend, "wwwroot");
+server.PublishWithContainerFiles(frontend, "wwwroot");
 
 builder.Build().Run();
